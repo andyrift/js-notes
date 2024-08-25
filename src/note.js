6c00c4266354ee540a161d31d1b63156e1c10655
Andy Rift <168359148+andyrift@users.noreply.github.com>
2024-08-25 23:35:29 +0300

    feat: add essential note class functionality

--- src/note.js
+++ src/note.js
@@ -1,5 +1,8 @@
 export class Note {
-    body
+    _body
+
+    // milliseconds
+    _timestamp
 
     set name(value) {
         if (typeof value !== 'string')
@@ -13,8 +16,29 @@ export class Note {
         return this._name
     }
 
-    constructor(name) {
+    set body(value) {
+        if (typeof value !== 'string')
+            throw new Error('A note body should be a string')
+        this._body = value
+    }
+
+    get body() {
+        return this._body
+    }
+
+    set timestamp(value) {
+        if (typeof value !== 'number')
+            throw new Error('A timestamp body should be a number')
+        this._timestamp = value
+    }
+
+    get timestamp() {
+        return this._timestamp
+    }
+
+    constructor(name, opts) {
         this.name = name
-        this.body = ''
+        this.body = opts && opts.body || ''
+        this.timestamp = opts && opts.timestamp || Date.now()
     }
 }
--- test/note.spec.js
+++ test/note.spec.js
@@ -3,35 +3,102 @@ import { Note } from '../src/note'
 describe('new note', () => {
 
     it('has name given to it', () => {
-        const note = new Note('Note name')
+        const note = new Note('Note')
 
-        expect(note.name).toBe('Note name')
+        expect(note.name).toBe('Note')
     })
 
     it('has empty body', () => {
-        const note = new Note('Note name')
+        const note = new Note('Note')
 
         expect(note.body).toBe('')
     })
 })
 
-describe('note constructor', () => {
+describe('note name', () => {
 
-    it('throws error when given no name', () => {
+    it('can be set and read', () => {
+        const note = new Note('Note')
+
+        note.name = 'New name'
+
+        expect(note.name).toBe('New name')
+    })
+
+    it('can not be empty', () => {
+        const note = new Note('Note')
+
+        function setName() { note.name = '' }
+
+        expect(setName).toThrow(/name.*empty/)
+    })
+
+    it('can only be a string', () => {
+        const note = new Note('Note')
+
+        function setName() { note.name = 0 }
+
+        expect(setName).toThrow(/name.*string/)
+    })
+
+    it('can not be initialized automatically', () => {
         function createNote() { new Note() }
 
-        expect(createNote).toThrowError(/name.*string/i)
+        expect(createNote).toThrow(/name.*string/i)
+    })
+})
+
+describe('note body', () => {
+
+    it('can be set and read', () => {
+        const note = new Note('Note')
+
+        note.body = 'note body'
+
+        expect(note.body).toBe('note body')
     })
 
-    it('throws error when given name of non-string type', () => {
-        function createNote() { new Note(0) }
+    it('can only be a string', () => {
+        const note = new Note('Note')
+        function setBody() { note.body = 0 }
 
-        expect(createNote).toThrowError(/name.*string/i)
+        expect(setBody).toThrow(/body.*string/)
     })
 
-    it('throws error when given empty string as name', () => {
-        function createNote() { new Note('') }
+    it('can be set upon creation', () => {
+        const note = new Note('Note', { body: 'note body' })
 
-        expect(createNote).toThrowError(/name.*empty/i)
+        expect(note.body).toBe('note body')
+    })
+
+})
+
+describe('note timestamp', () => {
+    beforeEach(() => {
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
+    })
+
+    it('is initialized as now', () => {
+        const note = new Note('Note')
+
+        expect(note.timestamp).toEqual(Date.now())
+    })
+
+    it('can be set upon creation', () => {
+        const note = new Note('Note', { timestamp: 1 })
+
+        expect(note.timestamp).toEqual(1)
+    })
+
+    it('can be set', () => {
+        const note = new Note('Note')
+
+        note.timestamp = 1
+
+        expect(note.timestamp).toEqual(1)
     })
 })
